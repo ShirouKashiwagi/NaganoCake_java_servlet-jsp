26263197a7e0ec26432ab05af209201c861e15ec
shiro <shiro@MSI>
2025-12-07 15:31:35 +0900

    【kashiwagi】ログイン画面の追加

---
+++ NaganoCake_java/src/main/java/com/naganocake/controller/customer/CustomerLoginComplete.java
@@ -0,0 +1,22 @@
+package com.naganocake.controller.customer;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class CustomerLoginComplete
+ */
+@WebServlet("/CustomerLoginComplete")
+public class CustomerLoginComplete extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private Member createMemberFromRequest
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
