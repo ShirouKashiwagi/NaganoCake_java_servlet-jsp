53e22ff6662656141e11ca5156012c87283db28a
ShirouKashiwagi <shirou0132@gmail.com>
2026-04-20 23:18:29 +0900

    【kashiwagi】Entityクラスの作成 - EntityとModelクラスの分別改修（序その１）

---
+++ NaganoCake_java/src/main/java/com/naganocake/entity/CartItemEntity.java
@@ -0,0 +1,66 @@
+package com.naganocake.entity;
+
+import java.time.LocalDateTime;
+
+public class CartItemEntity {
+		
+		  private int id;
+		  private int memberId;
+		  private int itemId;
+		  private int amount;
+		  private LocalDateTime createdAt;
+		  private LocalDateTime updatedAt;
+
+			// 不要 - カートに追加するのはCartItemではなくコントローラーの責務
+		//  public void add(CartItem cartItem) {
+		//  }
+
+		  public int getId() {
+		    return id;
+		  }
+
+		  public void setId(int id) {
+		    this.id = id;
+		  }
+
+		  public int getMemberId() {
+		    return memberId;
+		  }
+
+		  public void setMemberId(int memberId) {
+		    this.memberId = memberId;
+		  }
+
+		  public int getItemId() {
+		    return itemId;
+		  }
+
+		  public void setItemId(int itemId) {
+		    this.itemId = itemId;
+		  }
+
+		  public int getAmount() {
+		    return amount;
+		  }
+
+		  public void setAmount(int amount) {
+		    this.amount = amount;
+		  }
+
+		  public LocalDateTime getCreatedAt() {
+		    return createdAt;
+		  }
+
+		  public void setCreatedAt(LocalDateTime createdAt) {
+		    this.createdAt = createdAt;
+		  }
+
+		  public LocalDateTime getUpdatedAt() {
+		    return updatedAt;
+		  }
+
+		  public void setUpdatedAt(LocalDateTime updatedAt) {
+		    this.updatedAt = updatedAt;
+		  }
+
+}
